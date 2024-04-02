a043343a098c7ff85c1728af7371c2ed2601f90b
cami.brigueda@gmail.com <cami.brigueda@gmail.com>
2024-04-01 21:25:04 -0500

    Working on formatting the output image before integration with back-end development since it's incomplete as of right now; Also worked on the save_image function and error checking functionalities

--- gui.py
+++ gui.py
@@ -5,14 +5,16 @@ from pandastable import Table, TableModel
 import pandas as pd
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-from Database import *
+import os
+# from Database import *
+
 
 
 """
 Team Name: Team 6 - Analytical Database for TEM Sample Thickness Determination
 Company: Samsung Austin Semiconductor
 Team Members: Camila Brigueda, Angelo Carrion, Tejini Murthy
-Date: March 26, 2024
+Date: April 1, 2024
 
 This script creates a GUI application using tkinter for loading images and displaying simulation data in a table.
 """
@@ -45,18 +47,30 @@ def convert_to_tif(file_path):
 
 
 """
-    Opens a file dialog for the user to select an image file. The selected image is displayed in the provided label.
-    Also creates a new window for user to input parameters: Accelerating Voltage, Zone Axis, and Convergence Angle.
-    The input values are added to the table when the user clicks the Submit button.
+    Clears the canvas of any images. Opens a file dialog for the user to select an image file. 
+    The selected image is displayed in the provided label. Also creates a new window for user to input 
+    parameters: Accelerating Voltage, Zone Axis, and Convergence Angle. The input values are added to
+    the table when the user clicks the Submit button.
 
     Parameters:
     ----------
-    img_label : tk.Label
+    input_img_label : tk.Label
         The label in which the selected image will be displayed.
 """
 loaded_image = None
-def load_image(img_label):
+def load_image(input_img_label):
     global loaded_image
+
+    # Clear Canvas
+    for widget in input_img_label.winfo_children():
+        widget.destroy()
+
+    empty_image = tk.PhotoImage()
+    input_img_label.config(image = empty_image)
+    input_img_label.image = empty_image
+    input_img_label.config(image = empty_image)
+    input_img_label.image = empty_image
+
     file_path = filedialog.askopenfilename(filetypes = [("Image files", "*.png *.jpg *.jpeg *.tif *.tiff")])
 
     if not file_path:
@@ -68,23 +82,28 @@ def load_image(img_label):
             tif_file_path = convert_to_tif(file_path)
 
         img = Image.open(tif_file_path)
-        fig = Figure(figsize= (4,4))
+        fig = Figure(figsize =  (4,4))
         ax = fig.add_subplot(111)
         ax.imshow(img, cmap = 'gray')
         ax.axis('off')
 
-        canvas = FigureCanvasTkAgg(fig, master = img_label)
+        # Removes the white space around the image
+        fig.subplots_adjust(left = 0, right = 1, bottom = 0, top = 1)
+
+        canvas = FigureCanvasTkAgg(fig, master = input_img_label)
         canvas.draw()
         canvas.get_tk_widget().pack()
         loaded_image = ImageTk.PhotoImage(img)
-        output_img_label.config(image = loaded_image)
-        output_img_label.image = loaded_image
+        input_img_label.config(image = loaded_image)
+        input_img_label.image = loaded_image
 
 
     # Create a new window for user input
     input_window = tk.Toplevel(window)
     input_window.title("Input Parameters")
     input_window.geometry("300x300+350+300")
+
+    # input_window.bind('<Destroy>', lambda e: input_img_label.config(image = loaded_image))
 
 
     # Create labels and entry fields for each parameter
@@ -105,7 +124,7 @@ def load_image(img_label):
 
 
     # Create a button that will retrieve the input values when clicked
-    submit_button = tk.Button(input_window, text = "Submit", command = lambda: retrieve_input(voltage_entry, axis_entry, angle_entry, input_window, table, df, output_img_label))
+    submit_button = tk.Button(input_window, text = "Submit", command = lambda: retrieve_input(voltage_entry, axis_entry, angle_entry, input_window, table, df, input_img_label))
     submit_button.pack()
 
 
@@ -130,7 +149,7 @@ def load_image(img_label):
         The DataFrame that contains the data for the table.
 """
 voltage, axis, angle = "", "", ""
-def retrieve_input(voltage_entry, axis_entry, angle_entry, input_window, table, df, output_img_label):
+def retrieve_input(voltage_entry, axis_entry, angle_entry, input_window, table, df, input_img_label):
     global loaded_image
     global voltage
     global axis
@@ -145,14 +164,14 @@ def retrieve_input(voltage_entry, axis_entry, angle_entry, input_window, table, 
     if (voltage == "") or (axis == "") or (angle == ""):
         empty_image = tk.PhotoImage()
         loaded_image = empty_image
-        output_img_label.config(image = loaded_image)
-        output_img_label.image = loaded_image
+        input_img_label.config(image = loaded_image)
+        input_img_label.image = loaded_image
         messagebox.showerror("Error", "Please input all parameters")
         return
     else:
         # Check if the 'Simulation Measurements' titles exist in the DataFrame
-        measurements = ['Accelerating Voltage', 'Zone Axis', 'Convergence Angle']
-        results = [voltage, axis, angle]
+        measurements = ['Accelerating Voltage', 'Zone Axis', 'Convergence Angle', 'Material', 'Thickness']
+        results = [voltage, axis, angle, 'Silicon', '17 nm']
 
         for i, measurement in enumerate(measurements):
             if measurement in df['Simulation Measurements'].values:
@@ -163,18 +182,18 @@ def retrieve_input(voltage_entry, axis_entry, angle_entry, input_window, table, 
                 df = df.append({'Simulation Measurements': measurement, 'Simulation Results': results[i]}, ignore_index=True)
 
         # Calculate thickness here and update the DataFrame
-        thickness = determine_thickness()  # Assuming determine_thickness() returns the thickness
-        if 'Thickness' in df['Simulation Measurements'].values:
-            df.loc[df['Simulation Measurements'] == 'Thickness', 'Simulation Results'] = thickness
-        else:
-            df = df.append({'Simulation Measurements': 'Thickness', 'Simulation Results': thickness}, ignore_index=True)
+        # thickness = determine_thickness()  # Assuming determine_thickness() returns the thickness
+        # if 'Thickness' in df['Simulation Measurements'].values:
+        #     df.loc[df['Simulation Measurements'] == 'Thickness', 'Simulation Results'] = thickness
+        # else:
+        #     df = df.append({'Simulation Measurements': 'Thickness', 'Simulation Results': thickness}, ignore_index=True)
 
         # Update the table
         table.updateModel(TableModel(df))
         table.redraw()
 
-        output_img_label.config(image = loaded_image)
-        output_img_label.image = loaded_image
+        input_img_label.config(image = loaded_image)
+        input_img_label.image = loaded_image
 
         input_window.destroy()
 
@@ -182,25 +201,55 @@ def retrieve_input(voltage_entry, axis_entry, angle_entry, input_window, table, 
 
 def save_image():
     global loaded_image
-    if output_img_label.image is None:
-        messagebox.showerror("Error", "No image to save")
+    if loaded_image is None:
+        messagebox.showerror("Error", "No image to save.")
         return
     
     file_path = filedialog.asksaveasfilename(defaultextension = ".tif", filetypes = [("TIFF files", "*.tif")])
     if file_path:
-        output_img_label.image.save(file_path)
+        loaded_image.save(file_path)
+
+
+# This code will be changed later once I am able to integrate the back-end
+# development with the front-end development. This is simply for formatting
+# purposes.
+def output_image():
+    global loaded_image
+    image_path = '17 nm.tif'
+    if os.path.exists(image_path):
+        img = Image.open(image_path)
+        fig = Figure(figsize =  (4,4))
+        ax = fig.add_subplot(111)
+        ax.imshow(img)
+        ax.axis('off')
+
+        # Removes the white space around the image
+        fig.subplots_adjust(left = 0, right = 1, bottom = 0, top = 1)
+
+        canvas = FigureCanvasTkAgg(fig, master = output_img_label)
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+        loaded_image = ImageTk.PhotoImage(img)
+        output_img_label.config(image = loaded_image)
+        output_img_label.image = loaded_image
+
+        # loaded_image = Image.open(image_path)
+        # photo = ImageTk.PhotoImage(loaded_image)
+
+        # # Set the image to the label
+        # output_img_label.config(image = photo)
+        # output_img_label.image = photo
+    else:
+        messagebox.showerror("Error", "No image found.")
 
 
 
-def determine_thickness():
-    pass
-
-
-
+# Initialize the main window
 window = tk.Tk()
 window.grid_columnconfigure(0, weight = 1)
 window.grid_rowconfigure(0, weight = 10)
 window.grid_rowconfigure(1, weight = 1)
+window.grid_rowconfigure(2, weight = 1)
 window.title("Samsung Austin Semiconductor and Texas A&M University - TEM Sample Thickness Determination")
 
 
@@ -211,66 +260,42 @@ window.geometry("{0}x{1}+0+0".format(window_width, window_height))
 
 
 # Output frame for displaying the output image
-output_frame = tk.Frame(window)
-output_frame.grid(row = 0, column = 1, sticky = 'nsew')
-output_frame.grid_columnconfigure(0, weight = 1)
-output_frame.grid_rowconfigure(0, weight = 1)
+# output_frame = tk.Frame(window)
+# output_frame.grid(row = 0, column = 1, sticky = 'nsew')
+# output_frame.grid_columnconfigure(0, weight = 1)
+# output_frame.grid_rowconfigure(0, weight = 1)
 
 
-output_img_label = tk.Label(output_frame)
-output_img_label.grid(row = 0, column = 1, padx = 10, pady = 10, sticky = "ne")
-
-
-# Left frame for image loading
+# Left frame for input image loading and outputting image
 left_frame = tk.Frame(window)
 left_frame.grid(row = 0, column = 0, sticky = 'nsew')
 left_frame.grid_columnconfigure(0, weight = 1)
 left_frame.grid_rowconfigure(0, weight = 1)
 
 
-img_label = tk.Label(left_frame)
-img_label.grid(row = 0, column = 0, padx = 10, pady = 10, sticky = "nw")
+input_img_label = tk.Label(left_frame)
+input_img_label.grid(row = 0, column = 0, padx = 50, pady = 50, sticky = "nw")
 
-load_button = tk.Button(left_frame, text = "Load Image", relief = "raised", height = 5, width = 40, font = 15, command = lambda: load_image(img_label))
-load_button.grid(row = 1, column = 0, padx = 10, pady = 10, sticky = "w")
+output_img_label = tk.Label(left_frame)
+output_img_label.grid(row = 0, column = 0, padx = 50, pady = 50, sticky = "ne")
 
+load_button = tk.Button(left_frame, text = "Load Image", relief = "raised", height = 5, width = 40, font = 15, command = lambda: load_image(input_img_label))
+load_button.grid(row = 2, column = 0, padx = 10, pady = 10, sticky = "w")
 
-determine_thickness_button = tk.Button(left_frame, text = "Determine Thickness", relief = "raised", height = 5, width = 40, font = 15, command=determine_thickness)
-determine_thickness_button.grid(row = 1, column = 0, padx = 10, pady = 10, sticky = "e")
+###
+# Later on, I will customize the buttons. As a reminder "bg" is for background color and "fg" is for text color
+###
+
+determine_thickness_button = tk.Button(left_frame, text = "Determine Thickness", relief = "raised", height = 5, width = 40, font = 15, command = output_image)
+determine_thickness_button.grid(row = 2, column = 0, padx = 10, pady = 10, sticky = "e")
+
+save_button = tk.Button(left_frame, text = "Save Image", relief = "raised", height = 2, width = 15, font = 15, command = save_image)
+save_button.grid(row = 1, column = 0, padx = 10, pady = 10, sticky = "ne")
 
 
 # Right frame for table view
 right_frame = tk.Frame(window)
 right_frame.grid(row = 0, column = 1, sticky = 'nsew')
-
-
-# Save Image button
-save_button = tk.Button(output_frame, text="Save Image", command=save_image)
-save_button.grid(row=1, column=0, padx=10, pady=10, sticky="ne")
-
-
-
-# Replace 'input_image_file' with the actual image file or variable
-processed_image = pre_process_image('Exp_1')
-best_image, thickness = get_best_image(processed_image)
-
-# Convert the images to a format that can be used in a Tkinter label
-processed_tk_image = ImageTk.PhotoImage(Image.fromarray(processed_image))
-best_tk_image = ImageTk.PhotoImage(Image.fromarray(best_image))
-
-# Create Tkinter labels and set their image options to the converted images
-processed_image_label = tk.Label(window, image=processed_tk_image)
-processed_image_label.image = processed_tk_image  # keep a reference to prevent garbage collection
-processed_image_label.pack()  # add the label to the window
-
-best_image_label = tk.Label(window, image=best_tk_image)
-best_image_label.image = best_tk_image  # keep a reference to prevent garbage collection
-best_image_label.pack()  # add the label to the window
-
-# Display the thickness calculation
-thickness_label = tk.Label(window, text=f"Thickness: {thickness}")
-thickness_label.pack()
-
 
 
 data = {
